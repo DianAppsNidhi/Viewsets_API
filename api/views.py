06dd340675ad7dac3bc96242232d485aa076b580
DianAppsNidhi <nidhi.kaushik@dianapps.com>
2022-10-31 15:39:38 +0530

    Initial commit

---
+++ api/views.py
@@ -0,0 +1,43 @@
+from urllib import response
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+from .models import Course, CourseSerializer
+from rest_framework import mixins, generics
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import viewsets
+
+
+# Create your views here.
+
+class CourseViewset(viewsets.ViewSet):
+    def list(self, request):
+        courses = Course.objects.all()
+        serializer = CourseSerializer(courses, many = True)
+        return Response(serializer.data)
+
+   
+
+    def create(self, request):
+        serializer = CourseSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)  
+        return Response(serializer.errors)    
+
+
+    def CourseRetrieve(self, request, pk ):
+        try:
+            course = Course.objects.get(pk=pk)
+        except  course.DoseNotExist:
+            return Response(status = status.HTTP_404_NOT_FOUND)  
+
+        serializer = CourseSerializer(course)
+        return Response(serializer.data)
+
+
+    #  def CourseUpdate()   
+            
+
